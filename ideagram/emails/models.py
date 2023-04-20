8913817f740f4b649f640f1e793caea063cf64fc
erfan samimi <erf80sami@gmail.com>
2023-04-20 18:23:53 +0330

    add migration files

--- ideagram/emails/models.py
+++ ideagram/emails/models.py
@@ -8,7 +8,7 @@ class Email(models.Model):
         ("password_reset", "password_reset"),
         ("notification", "notification"),
     )
-    email_type = models.CharField(choices=EMAIL_TYPE_CHOICES)
+    email_type = models.CharField(choices=EMAIL_TYPE_CHOICES, max_length=50)
     date = models.DateTimeField(auto_now_add=True)
     subject = models.CharField(max_length=150)
     content = models.TextField()

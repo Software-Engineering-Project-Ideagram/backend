a32c70ef33bf43d454c9a4496b8d67076dbf8b6d
erfan samimi <erf80sami@gmail.com>
2023-04-20 18:10:25 +0330

    fix URLs and add media path to settings

--- config/django/base.py
+++ config/django/base.py
@@ -133,6 +133,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'EXCEPTION_HANDLER': 'ideagram.api.exception_handlers.drf_default_with_modifications_exception_handler',

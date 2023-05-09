c8dfd4f80f068646446d6069e8b9e241e1a588c0
mahroonoohi <mahroonoohi@gmail.com>
2023-05-08 23:39:39 -0700

    IdeaReport

--- ideagram/ideas/selectors.py
+++ ideagram/ideas/selectors.py
@@ -1,7 +1,20 @@
 from django.db.models import QuerySet
 
-from ideagram.ideas.models import Classification
+from ideagram.ideas.models import Classification, Idea
+from ideagram.users.models import BaseUser
 
 
 def get_all_classifications() -> QuerySet(Classification):
     return Classification.objects.all()
+
+
+def get_idea_by_uuid(*, uuid: str, user: BaseUser = None) -> Idea | None:
+    if user:
+        idea = Idea.objects.filter(uuid=uuid, profile__user=user)
+    else:
+        idea = Idea.objects.filter(uuid=uuid)
+
+    if idea.exists():
+        return idea.first()
+    else:
+        return None

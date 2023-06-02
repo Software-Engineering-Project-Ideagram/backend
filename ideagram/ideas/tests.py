50138fc1dec37400a9e35998a336cb8722748ac7
pouria <pouria.sameti80@gmail.com>
2023-06-02 14:01:56 +0330

    idea app -> tests.py -> TestIdeaUpdate -> setUP() and test_update_idea() was implemented. result = OK.

--- ideagram/ideas/tests.py
+++ ideagram/ideas/tests.py
@@ -41,3 +41,56 @@ class TestCreateIdea(TestCase):
 
         self.assertEqual(profile.pk, new_idea.profile_id)
 
+
+class TestIdeaUpdate(TestCase):
+
+    def setUp(self) -> None:
+        profile = Profile.objects.get(pk=4)
+        data = {
+            "classification": [
+                2
+            ],
+            "title": "all eyes on you",
+            "goal": "release music",
+            "abstract": "my song",
+            "description": "artist: smash into pieces",
+            "image": "",
+            "max_donation": 30000,
+            "show_likes": True,
+            "show_views": True,
+            "show_comments": True
+        }
+        self.idea = create_idea(profile=profile, data=data)
+
+    def test_update_idea(self):
+        idea = Idea.objects.get(pk=self.idea.pk)
+        title1 = idea.title
+        goal1 = idea.goal
+        abstract1 = idea.abstract
+        description1 = idea.description
+        max_donation1 = idea.max_donation
+
+        data = {
+            "classification": [
+                2
+            ],
+            "title": "get back",
+            "goal": "remix music",
+            "abstract": "my song, my life",
+            "description": "artist: Nine Assets",
+            "image": "",
+            "max_donation": 17000,
+            "show_likes": True,
+            "show_views": True,
+            "show_comments": True
+        }
+
+        update_idea(idea=idea, data=data)
+
+        self.assertNotEqual(title1, idea.title)
+        self.assertNotEqual(goal1, idea.goal)
+        self.assertNotEqual(abstract1, idea.abstract)
+        self.assertNotEqual(description1, idea.description)
+        self.assertNotEqual(max_donation1, idea.max_donation)
+
+

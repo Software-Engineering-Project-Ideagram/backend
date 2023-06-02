b73409d660691407bc06d14244cc88f694542ee2
pouria <pouria.sameti80@gmail.com>
2023-06-02 20:01:06 +0330

    idea app -> tests.py -> CollaborationRequestCreate -> setUp() was changed. result = OK.

--- ideagram/ideas/tests.py
+++ ideagram/ideas/tests.py
@@ -310,45 +310,51 @@ class TestFinancialStepUpdate(TestCase):
         self.assertNotEqual(priority1, self.fs.priority)
 
 
-# class CollaborationRequestCreate(TestCase):
-#
-#     def setUp(self) -> None:
-#         profile = Profile.objects.get(pk=3)
-#         data = {
-#             "classification": [
-#                 2
-#             ],
-#             "title": "I Will Survive",
-#             "goal": "release music. the best song of the century",
-#             "abstract": "a song. Classic & Cultural",
-#             "description": "artist: Gloria Gaynor",
-#             "image": "",
-#             "max_donation": 30000,
-#             "show_likes": True,
-#             "show_views": True,
-#             "show_comments": True
-#         }
-#         self.idea = create_idea(profile=profile, data=data)
-#
-#     def test_create_cr(self):
-#
-#         data = {
-#           "title": "villon player",
-#           "status": "full_time",
-#           "skills": "reading note and 10 years experience about music",
-#           "age": 22,
-#           "education": "jazz music",
-#           "description": "string",
-#           "salary": 15000
-#         }
-#
-#         cr = create_collaboration_request(idea=self.idea, data=data)
-#
-#         is_exists = CollaborationRequest.objects.filter(pk=cr.pk).exists()
-#         self.assertTrue(is_exists)
-#         self.assertEqual(cr.idea.pk, self.idea.pk)
-#
-#
+class CollaborationRequestCreate(TestCase):
+
+    def setUp(self) -> None:
+        base_user = BaseUser.objects.create_user(email="user1@gmail.com",
+                                                 password="user",
+                                                 is_active=True, is_admin=False)
+        profile = Profile.objects.create(user=base_user, username="user1", is_public=True, is_active=True,
+                                          is_banned=False)
+        class_music = Classification.objects.create(title='music')
+
+        data = {
+            "classification": [
+                class_music.pk
+            ],
+            "title": "I Will Survive",
+            "goal": "release music. the best song of the century",
+            "abstract": "a song. Classic & Cultural",
+            "description": "artist: Gloria Gaynor",
+            "image": "",
+            "max_donation": 30000,
+            "show_likes": True,
+            "show_views": True,
+            "show_comments": True
+        }
+        self.idea = create_idea(profile=profile, data=data)
+
+    def test_create_cr(self):
+
+        data = {
+          "title": "villon player",
+          "status": "full_time",
+          "skills": "reading note and 10 years experience about music",
+          "age": 22,
+          "education": "jazz music",
+          "description": "string",
+          "salary": 15000
+        }
+
+        cr = create_collaboration_request(idea=self.idea, data=data)
+
+        is_exists = CollaborationRequest.objects.filter(pk=cr.pk).exists()
+        self.assertTrue(is_exists)
+        self.assertEqual(cr.idea.pk, self.idea.pk)
+
+
 # class CollaborationRequestUpdate(TestCase):
 #
 #     def setUp(self) -> None:

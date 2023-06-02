dbd1e12089fcae7a476a425089cdbe6989808cad
Kasra <91476471+kasraSMD@users.noreply.github.com>
2023-06-02 13:03:09 +0330

    one test added into ProfileTest

--- ideagram/profiles/tests.py
+++ ideagram/profiles/tests.py
@@ -1,6 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
+from ideagram.common.models import Address
 from ideagram.profiles.models import Profile, Following
 from ideagram.profiles.services import follow_profile, register, update_user_profile, add_social_media_to_profile
 from ideagram.users.Exceptions import InvalidPassword
@@ -160,6 +161,21 @@ class ProfileTest(TestCase):
         profile2 = Profile.objects.create(user=baseuser2, username="user2", is_public=True, is_active=True,
                                           is_banned=True)
 
+        baseuser3 = BaseUser.objects.create_user(email="user3@gmail.com",
+                                                 password="user3",
+                                                 is_active=True, is_admin=False)
+
+        baseuser3.is_email_verified = True
+        baseuser3.save()
+
+        profile3 = Profile.objects.create(user=baseuser3, username="user3", is_public=True, is_active=True,
+                                          is_banned=True)
+        profile3.first_name = "user3firstName"
+        profile3.last_name = "user3lastName"
+        profile3.birth_date = "1400-11-20"
+        profile3.address = Address.objects.create(city="city1",state="state1")
+        profile3.save()
+
     def test_is_visible(self):
         baseuser1=BaseUser.objects.get(email="user1@gmail.com")
         baseuser2=BaseUser.objects.get(email="user2@gmail.com")
@@ -182,3 +198,14 @@ class ProfileTest(TestCase):
         profile2 = Profile.objects.get(user=baseuser2, username="user2")
         self.assertEqual(str(profile1),"user1@gmail.com >> user1")
         self.assertEqual(str(profile2),"user2@gmail.com >> user2")
+
+    def test_is_profile_complete(self):
+        baseuser1 = BaseUser.objects.get(email="user1@gmail.com")
+        baseuser2 = BaseUser.objects.get(email="user2@gmail.com")
+        profile1 = Profile.objects.get(user=baseuser1, username="user1")
+        profile2 = Profile.objects.get(user=baseuser2, username="user2")
+        baseuser3 = BaseUser.objects.get(email="user3@gmail.com")
+        profile3 = Profile.objects.get(user=baseuser3, username="user3")
+        self.assertFalse(profile1.is_profile_complete)
+        self.assertFalse(profile2.is_profile_complete)
+        self.assertTrue(profile3.is_profile_complete)

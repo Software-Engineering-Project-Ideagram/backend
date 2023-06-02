91886f04a69b84e53f505ed0e445fce2d3905366
Kasra <91476471+kasraSMD@users.noreply.github.com>
2023-06-02 18:36:36 +0330

    test does not exist idea added into ReportsTest

--- ideagram/reports/tests.py
+++ ideagram/reports/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 
+from ideagram.ideas.models import Idea, Classification
+from ideagram.ideas.services import create_idea
 from ideagram.profiles.models import Profile
-from ideagram.reports.services import create_profile_report
+from ideagram.reports.services import create_profile_report, create_idea_report
 from ideagram.users.models import BaseUser
 
 
@@ -26,26 +28,34 @@ class ReportsTest(TestCase):
                                           is_banned=False)
         profile2 = Profile.objects.create(user=baseuser2, username="user2", is_public=True, is_active=True,
                                           is_banned=True)
+        classification = Classification.objects.create(title="music")
+        data = {
+            "classification": [classification],
+            "title": "music for a album",
+            "goal": "for charity",
+            "abstract": "I want to make people happy"
+        }
+        idea = create_idea(profile=profile2, data=data)
 
     def test_does_not_exist_reported_profile(self):
-        data={
-            "profile_username":"user3",
+        data = {
+            "profile_username": "user3",
             "report_reasons": "Spam",
             "description": "Spam",
         }
-        reporter_profile=Profile.objects.get(username="user1")
+        reporter_profile = Profile.objects.get(username="user1")
         with self.assertRaises(ValueError):
-            profile_report=create_profile_report(reporter=reporter_profile, data=data)
+            profile_report = create_profile_report(reporter=reporter_profile, data=data)
 
     def test_exist_reported_profile(self):
-        data={
-            "profile_username":"user2",
+        data = {
+            "profile_username": "user2",
             "report_reasons": "Spam",
             "description": "Spam",
         }
-        reporter_profile=Profile.objects.get(username="user1")
-        reported_profile=Profile.objects.get(username="user2")
-        res=create_profile_report(reporter=reporter_profile, data=data)
+        reporter_profile = Profile.objects.get(username="user1")
+        reported_profile = Profile.objects.get(username="user2")
+        res = create_profile_report(reporter=reporter_profile, data=data)
 
         reported_profile2 = res.profile_id
         reporter_profile2 = res.reporter_id
@@ -53,3 +63,11 @@ class ReportsTest(TestCase):
         self.assertEqual(reported_profile.username, reported_profile2.username)
         self.assertEqual(reporter_profile.username, reporter_profile2.username)
 
+    def test_does_not_exist_idea(self):
+        idea = Idea.objects.get(title="music for a album")
+        reporter_profile = Profile.objects.get(username="user1")
+        data = {
+            "idea": 1111
+        }
+        with self.assertRaises(ValueError):
+            idea_report = create_idea_report(reporter=reporter_profile, data=data)
